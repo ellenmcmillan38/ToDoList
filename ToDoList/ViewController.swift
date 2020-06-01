16fe28274f12a9272dc2bdf6735d99429b632c53
Ellen <ellenmcmillan38@gmail.com>
2020-05-31 17:03:17 -0700

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Ellen McMillan on 5/31/20.
+//  Copyright © 2020 Ellen McMillan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
